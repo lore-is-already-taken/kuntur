d56b687d1a775c1524e9c114d4ccfbc36be1a542
lore <lorenzosaavedra.c@gmail.com>
2026-05-20 13:14:38 -0400

    feat(web): add embed package skeleton

---
+++ web/embed_test.go
@@ -0,0 +1,22 @@
+package web_test
+
+import (
+	"io/fs"
+	"testing"
+
+	"github.com/lore/kuntur/web"
+)
+
+func TestTemplatesFS_containsIndex(t *testing.T) {
+	_, err := fs.Stat(web.TemplatesFS, "templates/index.html")
+	if err != nil {
+		t.Fatalf("TemplatesFS does not contain templates/index.html: %v", err)
+	}
+}
+
+func TestStaticFS_exists(t *testing.T) {
+	_, err := fs.Stat(web.StaticFS, "static")
+	if err != nil {
+		t.Fatalf("StaticFS does not expose static directory: %v", err)
+	}
+}
